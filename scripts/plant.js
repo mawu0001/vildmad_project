b0f1828cf6a6aeef428154e172d6cbaaeb72e627
Marie Wulff <mawu0001@stud.kea.dk>
2024-02-14 11:34:26 +0100

    js upload

---
+++ scripts/plant.js
@@ -0,0 +1,9 @@
+// https://rlrnltlgmzclzpyumxli.supabase.co
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJscm5sdGxnbXpjbHpweXVteGxpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc3NjIxODUsImV4cCI6MjAyMzMzODE4NX0.C-m5yj5h1tcMxZ45T0rdWHQJW2wXoyWwA_4Ys8ibSS8
+fetch("")
+    .then(res=>res.json())
+    .then(showData)
+
+function(){
+
+}
