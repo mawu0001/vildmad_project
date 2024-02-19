9ba3e465ff7396bb591fac27b93884dce8ecfca7
Mathilde <maje0014@stud.kea.dk>
2024-02-19 17:34:22 +0100

    season coding + styling on recipes site

--- scripts/recipes.js
+++ scripts/recipes.js
@@ -41,17 +41,6 @@ function showData(recipes){
     });
 }
 
-// function showRecipes(recipes) {
-//     //fange temp
-//     const Template = document.querySelector(".recipes_template").content;
-//     //lav kopi
-//     const copy = Template.cloneNode(true);
-//     //ændre indhold
-//     copy.querySelector(".recipes_name").textContent = recipes.title_name;
-//     copy.querySelector(".recipes_img").src = recipes.img;
-//     //apende
-//     document.querySelector(".recipesContainer1").appendChild(copy);
-//   }
 
 function showRecipes(recipesJSON){
     let productClone;
@@ -70,80 +59,70 @@ function showRecipes(recipesJSON){
 
 
 //Intern menu
-// window.addEventListener("load", sidenVises);
+window.addEventListener("load", sidenVises);
 
-// //forkortelser
-// const menu1 = document.querySelector("#menu1");
-// const menu2 = document.querySelector("#menu2");
-// const menu3 = document.querySelector("#menu3");
-// const menu4 = document.querySelector("#menu4");
+//forkortelser
+const menu1 = document.querySelector("#menu1");
+const menu2 = document.querySelector("#menu2");
+const menu3 = document.querySelector("#menu3");
+const menu4 = document.querySelector("#menu4");
 
-// const menu1Knap = document.querySelector("#menu1Knap");
-// const menu2Knap = document.querySelector("#menu2Knap");
-// const menu3Knap = document.querySelector("#menu3Knap");
-// const menu4Knap = document.querySelector("#menu4Knap");
+const menu1Knap = document.querySelector("#menu1Knap");
+const menu2Knap = document.querySelector("#menu2Knap");
+const menu3Knap = document.querySelector("#menu3Knap");
+const menu4Knap = document.querySelector("#menu4Knap");
 
-// function sidenVises() {
-//   console.log("sidenVises");
-//   showMenu1();
-// }
+function sidenVises() {
+  showMenu1();
+}
 
-// function showMenu1() {
-//   console.log("Menu1 vises");
+function showMenu1() {
+  //Gør menu synlig
+  menu1.style.display = "inherit";
+  menu2.style.display = "none";
+  menu3.style.display = "none";
+  menu4.style.display = "none";
 
-//   //Gør menu synlig
-//   menu1.style.display = "inherit";
-//   menu2.style.display = "none";
-//   menu3.style.display = "none";
-//   menu4.style.display = "none";
+  //Styling af knappen
+  menu1Knap.firstElementChild.classList.add("selected");
+  menu2Knap.firstElementChild.classList = "CTA2";
+  menu3Knap.firstElementChild.classList = "CTA2";
+  menu4Knap.firstElementChild.classList = "CTA2";
+}
 
-//   //Styling af knappen
-//   menu1Knap.firstElementChild.classList.add("selected");
-//   menu2Knap.firstElementChild.classList = "CTA2";
-//   menu3Knap.firstElementChild.classList = "CTA2";
-//   menu4Knap.firstElementChild.classList = "CTA2";
-// }
+function showMenu2() {
+  menu1.style.display = "none";
+  menu2.style.display = "inherit";
+  menu3.style.display = "none";
+  menu4.style.display = "none";
 
-// function showMenu2() {
-//   console.log("Menu2 vises");
+  //Styling af knappen
+  menu1Knap.firstElementChild.classList = "CTA2";
+  menu2Knap.firstElementChild.classList.add("selected");
+  menu3Knap.firstElementChild.classList = "CTA2";
+  menu4Knap.firstElementChild.classList = "CTA2";
+}
+function showMenu3() {
+  menu1.style.display = "none";
+  menu2.style.display = "none";
+  menu3.style.display = "inherit";
+  menu4.style.display = "none";
 
-//   menu1.style.display = "none";
-//   menu2.style.display = "inherit";
-//   menu3.style.display = "none";
-//   menu4.style.display = "none";
+  //Styling af knappen
+  menu1Knap.firstElementChild.classList = "CTA2";
+  menu2Knap.firstElementChild.classList = "CTA2";
+  menu3Knap.firstElementChild.classList.add("selected");
+  menu4Knap.firstElementChild.classList = "CTA2";
+}
+function showMenu4() {
+  menu1.style.display = "none";
+  menu2.style.display = "none";
+  menu3.style.display = "none";
+  menu4.style.display = "inherit";
 
-//   //Styling af knappen
-//   //Styling af knappen
-//   menu1Knap.firstElementChild.classList = "CTA2";
-//   menu2Knap.firstElementChild.classList.add("selected");
-//   menu3Knap.firstElementChild.classList = "CTA2";
-//   menu4Knap.firstElementChild.classList = "CTA2";
-// }
-// function showMenu3() {
-//   console.log("Menu3 vises");
-
-//   menu1.style.display = "none";
-//   menu2.style.display = "none";
-//   menu3.style.display = "inherit";
-//   menu4.style.display = "none";
-
-//   //Styling af knappen
-//   menu1Knap.firstElementChild.classList = "CTA2";
-//   menu2Knap.firstElementChild.classList = "CTA2";
-//   menu3Knap.firstElementChild.classList.add("selected");
-//   menu4Knap.firstElementChild.classList = "CTA2";
-// }
-// function showMenu4() {
-//   console.log("Menu4 vises");
-
-//   menu1.style.display = "none";
-//   menu2.style.display = "none";
-//   menu3.style.display = "none";
-//   menu4.style.display = "inherit";
-
-//   //Styling af knappen
-//   menu1Knap.firstElementChild.classList = "CTA2";
-//   menu2Knap.firstElementChild.classList = "CTA2";
-//   menu3Knap.firstElementChild.classList = "CTA2";
-//   menu4Knap.firstElementChild.classList.add("selected");
-// }
+  //Styling af knappen
+  menu1Knap.firstElementChild.classList = "CTA2";
+  menu2Knap.firstElementChild.classList = "CTA2";
+  menu3Knap.firstElementChild.classList = "CTA2";
+  menu4Knap.firstElementChild.classList.add("selected");
+}
