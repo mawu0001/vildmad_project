1842696b488f07c063a5ce488ce9f500668d951f
Jacob <jacobvestergaard2@gmail.com>
2024-02-20 14:51:35 +0100

    prøver at lave single view js

--- scripts/plant.js
+++ scripts/plant.js
@@ -1,7 +1,7 @@
 const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
 
-fetch(`https://rlrnltlgmzclzpyumxli.supabase.co/rest/v1/plants${id}`, {
+fetch(`https://rlrnltlgmzclzpyumxli.supabase.co/rest/v1/plants`, {
   method: "GET",
   headers: {
     apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJscm5sdGxnbXpjbHpweXVteGxpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc3NjIxODUsImV4cCI6MjAyMzMzODE4NX0.C-m5yj5h1tcMxZ45T0rdWHQJW2wXoyWwA_4Ys8ibSS8",
@@ -12,8 +12,9 @@ fetch(`https://rlrnltlgmzclzpyumxli.supabase.co/rest/v1/plants${id}`, {
 
 function showPlant(plant) {
   console.log(plant);
-  document.querySelector(".plant_description h1").textContent = plant.titel;
-  document.querySelector(".plant_description p").textContent = plant.description;
+  document.querySelector(".plant_titel").textContent = plant.title;
+  //   document.querySelector(".p_description").textContent = plant.description;
+
   // document.querySelector(".description_img")
 }
 
